78d14a6f8cfe9ed852a047e192da87739405bcc5
Ariel Davis <amanichai100@gmail.com>
2022-12-09 15:55:31 -0500

    modal bootstrap

--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -1,11 +1,29 @@
+import { useState } from "react";
 import "./ProjectCard.css";
 
+import ProjectCardModel from "./ProjectCardModel";
+
 const ProjectCard = ({ data }) => {
+  const [modalShow, setModalShow] = useState(false);
+
+  const modalClickHandler = () => {
+    if (data.name !== "WIP") {
+      setModalShow(true);
+    }
+  };
   return (
-    <div className="project-card__container">
-      <img className="project-card__img" src={data.img} alt="Filler" />
-      <div className="project-card__overlay">{data.name}</div>
-    </div>
+    <>
+      <div className="project-card__container" onClick={modalClickHandler}>
+        <img className="project-card__img" src={data.img} alt="Filler" />
+        <div className="project-card__overlay">{data.name}</div>
+      </div>
+
+      <ProjectCardModel
+        data={data}
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </>
   );
 };
 
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,43 +1,44 @@
 import "./Projects.css";
 
 import noImage from "../../assets/no-image.jpg";
+import gemTrader from "../../assets/gem-trader.png";
 
 import ProjectCard from "./ProjectCard";
 
 const Projects = () => {
   const projectData = [
     {
-      name: "test",
+      name: "Luck of Dice",
       img: noImage,
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
     {
-      name: "test",
+      name: "Gem Trader",
+      img: gemTrader,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    {
+      name: "Forecast Planner",
       img: noImage,
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
     {
-      name: "test",
+      name: "Dev Learning",
       img: noImage,
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
     {
-      name: "test",
+      name: "WIP",
       img: noImage,
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
     {
-      name: "test",
-      img: noImage,
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    },
-    {
-      name: "test",
+      name: "WIP",
       img: noImage,
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
