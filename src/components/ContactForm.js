6a14cf73361a0b5bddaa8cdba58b870b66ac44a5
Ariel Davis <amanichai100@gmail.com>
2022-12-09 12:20:58 -0500

    form boiler

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,7 +1,26 @@
 import "./ContactForm.css";
 
 const ContactForm = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="container">
+      <form action="" className="contact-form__form">
+        <div className="contact-form__top">
+          <div>
+            <label htmlFor="">Full Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Email</label>
+            <input type="email" />
+          </div>
+        </div>
+        <div>
+          <label htmlFor="">Message</label>
+          <textarea cols="30" rows="10"></textarea>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default ContactForm;
